1d9751b2ee92a4e839650e2fdc96b6929bffc514
Hugo_Navarro <tehspartanhugo@outlook.com>
2026-04-29 18:54:43 +0200

    Editado los archivos Jugador.java y Equipo.java

--- src/main/java/com/hugonavarro/ejerciciosdocumentacion/futbol/Equipo.java
+++ src/main/java/com/hugonavarro/ejerciciosdocumentacion/futbol/Equipo.java
@@ -1,4 +1,52 @@
 package com.hugonavarro.ejerciciosdocumentacion.futbol;
 
+/**
+ * Clase que representa a un equipo de fútbol.
+ */
 public class Equipo {
+    private String nombre;
+    private Jugador[] jugadores;
+    private int goles;
+
+    /**
+     * Constructor de la clase Equipo.
+     * @param nombre Nombre de un equipo
+     * @param jugadores Array de 11 jugadores que debe tener un equipo
+     * @throws IllegalArgumentException Si un equipo no tuviese exactamente 11 jugadores
+     */
+    public Equipo(String nombre, Jugador[] jugadores) {
+        if (jugadores.length != 11) {
+            throw new IllegalArgumentException("El equipo debe tener 11 jugadores");
+        }
+
+        this.nombre = nombre;
+        this.jugadores = jugadores;
+        this.goles = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Jugador[] getJugadores() {
+        return jugadores;
+    }
+
+    public Jugador getJugador(int indice) {
+        return jugadores[indice];
+    }
+
+    public void intercambiarJugadores(int i, int j) {
+        Jugador temp = jugadores[i];
+        jugadores[i] = jugadores[j];
+        jugadores[j] = temp;
+    }
+
+    public int getGoles() {
+        return goles;
+    }
+
+    public void marcarGol() {
+        goles++;
+    }
 }
--- src/main/java/com/hugonavarro/ejerciciosdocumentacion/futbol/Jugador.java
+++ src/main/java/com/hugonavarro/ejerciciosdocumentacion/futbol/Jugador.java
@@ -1,4 +1,67 @@
 package com.hugonavarro.ejerciciosdocumentacion.futbol;
 
+/**
+ * Clase que representa a un jugador de fútbol.
+ */
 public class Jugador {
+    private int dorsal;
+    private String nombreCompleto;
+    private int portero;
+    private int defensa;
+    private int regate;
+    private int pase;
+    private int tiro;
+
+    /**
+     * Constructor de la clase Jugador.
+     * @param dorsal Dorsal de un jugador
+     * @param nombreCompleto Nombre y apellidos de un jugador
+     * @param portero Habilidad de un jugador como portero (0-100)
+     * @param defensa Habilidad de un jugador que defiende (0-100)
+     * @param regate Habilidad de un jugador que regatea (0-100)
+     * @param pase Habilidad de un jugador que realiza pases (0-100)
+     * @param tiro Habilidad de un jugador que realiza tiros (0-100)
+     */
+    public Jugador(int dorsal, String nombreCompleto,  int portero, int defensa, int regate, int pase, int tiro) {
+        this.dorsal = dorsal;
+        this.nombreCompleto = nombreCompleto;
+        this.portero = portero;
+        this.defensa = defensa;
+        this.regate = regate;
+        this.pase = pase;
+        this.tiro = tiro;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public int getPortero() {
+        return portero;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public int getRegate() {
+        return regate;
+    }
+
+    public int getPase() {
+        return pase;
+    }
+
+    public int getTiro() {
+        return tiro;
+    }
+
+    @Override
+    public String toString() {
+        return nombreCompleto + " (#" + dorsal + ")";
+    }
 }
