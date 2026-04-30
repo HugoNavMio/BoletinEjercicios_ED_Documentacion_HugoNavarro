b2e6bd66a7b19619936689352e1c51e1baf21bc7
Hugo_Navarro <tehspartanhugo@outlook.com>
2026-04-30 19:33:44 +0200

    Editado el build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("net.datafaker:datafaker:2.5.4")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.test {
